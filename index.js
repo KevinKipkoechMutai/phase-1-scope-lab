e682522de3a25b34b0f7d3f675f5f6a7bc8fade7
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-22 19:56:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+//customerName declaration
+var customerName = "bob";
+
+//function to access the global variable and uppercase it
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+//bestCustomer declaration
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+//overwriteBestCustomer
+function overwriteBestCustomer() {
+    return bestCustomer = "maybe bob";
+}
+
+//leastFavoriteCustomer
+const leastFavoriteCustomer = "james";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jimmy";
+}
